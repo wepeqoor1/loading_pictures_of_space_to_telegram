31c4c06e2a2f4744bb12ad38724fb56ba8013708
wepeqoor1 <hoopeeeee@gmail.com>
2022-06-28 11:44:41 +0300

    Add last Spacex lanch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from pprint import pprint
 
 
 def download_picture(url: str, path: str) -> None:
@@ -10,16 +11,40 @@ def download_picture(url: str, path: str) -> None:
         write_file.write(response.content)
 
 
+def fetch_spacex_last_launch(dir_images: str) -> None:
+    """Get last images links from Spacex flight"""
+    api_spacex_data = 'https://api.spacexdata.com/v5/launches/past'
+    response = requests.get(api_spacex_data)
+    response.raise_for_status()
+    response = response.json()
+
+    flight_number = response[-1]['flight_number']
+    company_name = 'spacex'
+
+    for launch in response:
+        if picture_links := launch['links']['flickr']['original']:
+            for idx_link, picture_link in enumerate(picture_links):
+                picture_name: str = f'{company_name}_{idx_link}.jpg'
+                path = ''.join([dir_images, picture_name])
+                
+                download_picture(url=picture_link, path=path)
+            break
+        else:
+            flight_number -= 1
+
+
 if __name__ == '__main__':
 
+    picture_name = 'hubble.jpeg'
     dir_images = 'images/'
+
     if not os.path.exists(dir_images):
         os.makedirs(dir_images)
     
+    path_images: str = ''.join([dir_images, picture_name])
     url: str = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-    path = ''.join([dir_images, 'hubble.jpeg'])
 
     try:
-        download_picture(url=url, path=path)
+        fetch_spacex_last_launch(dir_images=dir_images)
     except requests.exceptions.HTTPError:
-        print('Не удалось загрузить картинку')
+        print('Ошибка в в получении картинки')
