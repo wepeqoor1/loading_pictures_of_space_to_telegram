552b5619f6d97fc95d027eab3c3872015720d5a1
wepeqoor1 <hoopeeeee@gmail.com>
2022-07-01 01:21:47 +0300

    Replace directiry path to global variable

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -8,9 +8,9 @@ from word_processing import get_image_format
 from file_operations import check_directory
 
 
-def fetch_images_flight_number(dir_images: str, flight_number: int):
+def fetch_images_flight_number(flight_number: int):
     """Downloading images from flight number SpaceX launch"""
-    
+
     url = f'https://api.spacexdata.com/v3/launches/{flight_number}'
     response: requests.Response = requests.get(url)
     response.raise_for_status()
@@ -19,10 +19,10 @@ def fetch_images_flight_number(dir_images: str, flight_number: int):
     if not (image_links := response["links"]["flickr_images"]) or response.get('error'):
         return NotFoundErr
     
-    iterate_over_links(image_links=image_links, dir_images=dir_images)
+    iterate_over_links(image_links=image_links)
 
 
-def fetch_images_last_launch(dir_images: str, flight_number=None) -> None:
+def fetch_images_last_launch(flight_number=None) -> None:
     """Downloading images from last SpaceX launch"""
         
     url = "https://api.spacexdata.com/v5/launches/past"
@@ -34,17 +34,17 @@ def fetch_images_last_launch(dir_images: str, flight_number=None) -> None:
 
     for launch in response:
         if image_links := launch["links"]["flickr"]["original"]:
-            iterate_over_links(image_links, dir_images)
+            iterate_over_links(image_links=image_links)
             break
         else:
             flight_number -= 1
     
     
-def iterate_over_links(image_links: list, dir_images: str):
+def iterate_over_links(image_links: list):
     for idx_link, image_link in enumerate(image_links):
                 image_format = get_image_format(url=image_link)
                 image_name: str = f"spacex_{idx_link}{image_format}"
-                get_image(url=image_link, path="".join([dir_images, image_name]))
+                get_image(url=image_link, path="".join([config.dir_images, image_name]))
     
 
 def console_argument_parser():
@@ -61,7 +61,7 @@ def console_argument_parser():
 
 
 if __name__ == '__main__':
-    check_directory(config.dir_images)
+    check_directory()
     
     args = console_argument_parser()
     flight_number: int = args.id
@@ -69,7 +69,6 @@ if __name__ == '__main__':
     try:
         if flight_number: 
             fetch_images_flight_number(
-                dir_images=config.dir_images,
                 flight_number=flight_number
                 )
             print(f'Фотографии с запуска ракеты №{flight_number} скачены')
@@ -78,5 +77,3 @@ if __name__ == '__main__':
             print('Фотографии с последнего запуска ракеты скачены')
     except requests.exceptions.HTTPError as er:
         print('Данные не найдены')
-    else:
-        print('Картинки загружены')
