1b14dd6f13bd8e0e670a88f761c98ec1cd8bb64f
wepeqoor1 <hoopeeeee@gmail.com>
2022-07-02 20:13:20 +0300

    Timeout publish to telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,7 @@
 import os
 import random
+from time import sleep
+
 import telegram
 
 from environment_variables import load_environment_variables
@@ -7,34 +9,47 @@ import config
 
 
 class TelegramBot:
-    def __init__(self, bot):
+    def __init__(self, bot, chat_id):
         self.bot = bot
-        self.chat_id = self.get_updates()
-
-    def get_me(self):
-        print(self.bot.get_me())
+        self.chat_id = chat_id
 
     def get_updates(self):
-        updates = self.bot.get_updates()
-        print(updates[-1])
+        updates = self.bot.get_updates()[-1]
+        print(updates)
+        
+    def send_message(self, message):
+        self.bot.send_message(text=message, chat_id=self.chat_id)
 
-    def send_message(self, message, chat_id):
-        self.bot.send_message(text=message, chat_id=chat_id)
+    def upload_image(self, image_path: str):
+        self.bot.send_document(chat_id=self.chat_id, document=open(image_path, 'rb'))
 
-    def upload_picture(self, chat_id: int, image_path: str):
-        self.bot.send_document(chat_id=chat_id, document=open(image_path, 'rb'))
+
+def generate_shuffle_images() -> list:
+    images: list = os.listdir(config.dir_images)
+    random.shuffle(images)
+    return images
 
 
 if __name__ == "__main__":
     load_environment_variables()
     
-    chat_id = os.getenv("TELEGRAM_CHAT_ID")
-    message = 'Картинка дня'
-    image_path = "".join(
-        [config.dir_images, random.choice(os.listdir(config.dir_images))]
-    )
+    chat_id: int = -1001645491537
+    message: str = 'Мой прекрасный космос'
+    seconds_in_hour: int = 3600
+    publish_seconds_timeout: int = config.telegram_hours_timeout_publish * seconds_in_hour
 
-    bot = telegram.Bot(token=os.getenv("TELEGRAM_API_TOKEN"))
-    telegram_bot = TelegramBot(bot=bot)
-    send_message = telegram_bot.send_message(message=message, chat_id=chat_id)
-    telegram_bot.upload_picture(chat_id=chat_id, image_path=image_path)
+    bot: telegram.Bot = telegram.Bot(token=os.getenv("TELEGRAM_API_TOKEN"))
+    telegram_bot: TelegramBot = TelegramBot(bot=bot, chat_id=chat_id)
+    
+    shuffle_images = []
+    
+    while True:
+        if not shuffle_images:
+            shuffle_images: list = generate_shuffle_images()
+        image: str = shuffle_images.pop()        
+        image_path: str = "".join([config.dir_images, image])
+        
+        telegram_bot.send_message(message=message)
+        telegram_bot.upload_image(image_path=image_path)
+        
+        sleep(publish_seconds_timeout)
