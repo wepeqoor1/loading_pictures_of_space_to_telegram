864373e16de44bb2168a11731761cfcb5e86eb1f
wepeqoor1 <hoopeeeee@gmail.com>
2022-07-10 16:01:39 +0300

    Broke the functions into parts

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,53 +1,40 @@
-from xml.dom import NotFoundErr
+import os
 import requests
 import argparse
+import urllib.parse
 
 import config
-from get_data import get_image
-from word_processing import get_image_format
-from file_operations import check_directory
+from file_operations import check_directory, save_image
 
 
-def fetch_images_flight_number(flight_number: int):
+def fetch_data_by_flight_number(flight_number: int) -> dict:
     """Downloading images from flight number SpaceX launch"""
 
     url = f"https://api.spacexdata.com/v3/launches/{flight_number}"
     response: requests.Response = requests.get(url)
     response.raise_for_status()
-    response: dict = response.json()
-
-    if not (image_links := response["links"]["flickr_images"]) or response.get("error"):
-        return NotFoundErr
-
-    iterate_over_links(image_links=image_links)
+    
+    return response.json()
 
 
-def fetch_images_last_launch(flight_number=None) -> None:
+def fetch_data_by_last_launch() -> dict:
     """Downloading images from last SpaceX launch"""
 
     url = "https://api.spacexdata.com/v5/launches/past"
     response: requests.Response = requests.get(url)
     response.raise_for_status()
-    response = response.json()
-
-    flight_number = response[-1]["flight_number"]
-
-    for launch in response:
-        if image_links := launch["links"]["flickr"]["original"]:
-            iterate_over_links(image_links=image_links)
-            break
-        else:
-            flight_number -= 1
+    
+    return response.json()
 
 
-def iterate_over_links(image_links: list):
-    for idx_link, image_link in enumerate(image_links):
-        image_format = get_image_format(url=image_link)
-        image_name: str = f"spacex_{idx_link}{image_format}"
-        get_image(url=image_link, path=f"{config.dir_images}{image_name}")
+def get_image_format(url: str) -> str:
+    url_with_decode_spaces = urllib.parse.unquote(url)
+    path_in_url = urllib.parse.urlsplit(url_with_decode_spaces).path
+    
+    return os.path.splitext(path_in_url)[-1]
 
 
-def parsing_console_argument():
+def parsing_console_arguments():
     parser = argparse.ArgumentParser(
         description=(
             """
@@ -67,15 +54,33 @@ def parsing_console_argument():
 if __name__ == "__main__":
     check_directory()
 
-    args = parsing_console_argument()
+    args = parsing_console_arguments()
     flight_number: int = args.id
 
     try:
         if flight_number:
-            fetch_images_flight_number(flight_number=flight_number)
-            print(f"Фотографии с запуска ракеты №{flight_number} скачены")
+            flight: dict = fetch_data_by_flight_number(flight_number=flight_number)
+            if not flight["links"]["flickr_images"] or flight.get("error"):
+                raise requests.exceptions.HTTPError
+            else:
+                image_links = flight["links"]["flickr_images"]
         else:
-            fetch_images_last_launch(config.dir_images)
-            print("Фотографии с последнего запуска ракеты скачены")
+            flights: dict = fetch_data_by_last_launch()
+            flight_number = flights[-1]["flight_number"]
+            
+            for flight in flights:
+                if not flight ["links"]["flickr"]["original"]:
+                    flight_number -= 1
+                    continue
+                if image_links := flight["links"]["flickr"]["original"]:
+                    break
+                
+        for idx, image_link in enumerate(image_links):
+            image_format = get_image_format(url=image_link)
+            image_name: str = f"spacex_{idx}{image_format}"
+            image = requests.get(image_link)
+            image.raise_for_status()
+            save_image(response=image, path=f'{config.dir_images}{image_name}{image_format}')
+            
     except requests.exceptions.HTTPError as er:
         print("Данные не найдены")
