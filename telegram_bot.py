85972313841600e15db7cb3a2e5ed22803d0461f
wepeqoor1 <hoopeeeee@gmail.com>
2022-07-10 18:23:30 +0300

    Remove TelegramBot class and past in ifname

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,22 +8,6 @@ from dotenv import load_dotenv
 import config
 
 
-class TelegramBot:
-    def __init__(self, bot, chat_id):
-        self.bot = bot
-        self.chat_id = chat_id
-
-    def get_updates(self):
-        return self.bot.get_updates()[-1]
-
-    def send_message(self, message):
-        self.bot.send_message(text=message, chat_id=self.chat_id)
-
-    def upload_image(self, image_path: str):
-        with open(image_path, 'rb') as image:    
-            self.bot.send_document(chat_id=self.chat_id, document=image)
-
-
 def generate_mixed_images() -> list:
     images: list = os.listdir(config.dir_images)
     random.shuffle(images)
@@ -33,15 +17,14 @@ def generate_mixed_images() -> list:
 if __name__ == "__main__":
     load_dotenv()
 
-    chat_id = os.getenv('TELEGRAM_CHAT_ID')
-    message= "Мой прекрасный космос"
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+    message = "Мой прекрасный космос"
     seconds_in_hour = 3600
     publish_seconds_timeout: int = (
         config.telegram_hours_timeout_publish * seconds_in_hour
     )
 
     bot: telegram.Bot = telegram.Bot(token=os.getenv("TELEGRAM_API_TOKEN"))
-    telegram_bot: TelegramBot = TelegramBot(bot=bot, chat_id=chat_id)
 
     images = []
 
@@ -49,9 +32,9 @@ if __name__ == "__main__":
         if not images:
             images: list = generate_mixed_images()
         image_name: str = images.pop()
-        image_path: str = f'{config.dir_images}{image_name}'
-
-        telegram_bot.send_message(message=message)
-        telegram_bot.upload_image(image_path=image_path)
+        image_path: str = f"{config.dir_images}{image_name}"
+        bot.send_message(text=message, chat_id=chat_id)
+        with open(image_path, "rb") as image:
+            bot.send_document(chat_id=chat_id, document=image)
 
         sleep(publish_seconds_timeout)
