f0bf2257ef0f837b3e97d253c91ffab57257d641
egor.kochergin <egor.kochergin@aisa.ru>
2022-07-13 07:59:00 +0300

    Resend telegram post if bad connection

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,6 +4,10 @@ from time import sleep
 
 import telegram
 from dotenv import load_dotenv
+from telegram.error import NetworkError
+
+
+SECONDS_IN_HOUR = 3600
 
 
 def generate_mixed_images(dir_images) -> list:
@@ -19,20 +23,28 @@ if __name__ == "__main__":
     hours_timeout_publish = 4
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
     message = "Мой прекрасный космос"
-    seconds_in_hour = 3600
-    publish_seconds_timeout: int = hours_timeout_publish * seconds_in_hour
+    publish_seconds_timeout: int = hours_timeout_publish * SECONDS_IN_HOUR
 
     bot: telegram.Bot = telegram.Bot(token=os.getenv("TELEGRAM_API_TOKEN"))
 
     images = []
+    timeout = 1
 
     while True:
         if not images:
             images: list = generate_mixed_images(dir_images=dir_images)
-        image_name: str = images.pop()
-        image_path: str = f"{dir_images}{image_name}"
-        bot.send_message(text=message, chat_id=chat_id)
-        with open(image_path, "rb") as image:
-            bot.send_document(chat_id=chat_id, document=image)
+            image_name: str = images.pop()
+            image_path: str = f"{dir_images}{image_name}"
+            
+        try:
+            bot.send_message(text=message, chat_id=chat_id)
+            with open(image_path, "rb") as image:
+                bot.send_document(chat_id=chat_id, document=image)
+            sleep(publish_seconds_timeout)
+        except NetworkError as error:
+            sleep(timeout)
+            timeout = 10
+            print(f'Попытка повторной отправки через {timeout} секунд')
 
-        sleep(publish_seconds_timeout)
+            
+        
